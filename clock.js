f518baf187ddf4bbc23d754472e428f4fe6ca7cb
Brett <brettspencr@gmail.com>
2018-01-25 12:00:23 -0800

    Built the clock application

---
+++ clock.js
@@ -0,0 +1,47 @@
+//Get HTML references to clock hands
+var secondHand = document.getElementById("second");
+var minuteHand = document.getElementById("minute");
+var hourHand = document.getElementById("hour");
+
+
+//Calculate degree changes for each second. How many seconds does it take for each hand to rotate 360 degrees?
+var secondDeg = 360 / 60;
+var minuteDeg = 360 / (60 * 60);
+var hourDeg = 360 / (60 * 60 * 12);
+
+
+//Store current positions of clock hands. Translate current minutes, hours into seconds.
+//Then multiply # of seconds already passed by the degree change per second.
+var myDate = new Date();s
+var currSecondPos = myDate.getSeconds() * secondDeg;
+var currMinutePos = ((myDate.getMinutes() * 60) + myDate.getSeconds()) * minuteDeg;
+var currHourPos = ((myDate.getHours() * 60 * 60) + (myDate.getMinutes() * 60) + myDate.getSeconds()) * hourDeg;
+
+
+//Set starting positions for clock hands
+secondHand.style.transform = "rotate(" + currSecondPos + "deg)";
+minuteHand.style.transform = "rotate(" + currMinutePos + "deg)";
+hourHand.style.transform = "rotate(" + currHourPos + "deg)";
+
+
+//Define increment functions for each clock hand
+function incrementSecond() {
+	currSecondPos += secondDeg;
+	if (currSecondPos % 360 === 0) { currSecondPos = 0 }
+	secondHand.style.transform = "rotate(" + currSecondPos + "deg)";
+}
+function incrementMinute() {
+	currMinutePos += minuteDeg;
+	minuteHand.style.transform = "rotate(" + currMinutePos + "deg)";
+}
+function incrementHour() {
+	currHourPos += hourDeg;
+	hourHand.style.transform = "rotate(" + currHourPos + "deg)";
+}
+
+
+//Set intervals to increment each hand every second
+var secondInterval = setInterval(incrementSecond, 1000);
+var minuteInterval = setInterval(incrementMinute, 1000);
+var hourInterval = setInterval(incrementHour, 1000);
+
